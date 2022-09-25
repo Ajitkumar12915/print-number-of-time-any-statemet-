343f4819cdcf830ac0c8959bf471130b8db5c262
Ajit kumar <97993300+Ajitkumar12915@users.noreply.github.com>
2022-09-25 23:46:09 +0530

    Update print command.py

--- print command.py
+++ print command.py
@@ -1,5 +1,5 @@
-''' here in the bracket three value is used, first show startin, 
-second show ending point and last plurs one show step(how much 
+''' here in the bracket three value is used, first show starting, 
+second show ending point and last plus one show step(how much 
 you want to jump the step)'''
 for i in range(1,1100,+1):
     print("Maa") 
