49c804b4a750f713238dc90f54923079fd845f4b
Ajit kumar <97993300+Ajitkumar12915@users.noreply.github.com>
2022-09-25 23:45:01 +0530

    Add files via upload

---
+++ print command.py
@@ -0,0 +1,6 @@
+''' here in the bracket three value is used, first show startin, 
+second show ending point and last plurs one show step(how much 
+you want to jump the step)'''
+for i in range(1,1100,+1):
+    print("Maa") 
+    print("YADAV G")                             
